c37da4c80229be846735ccb1f912165bfe653972
Eric Mouledoux <EricMouledoux@gmail.com>
2018-02-23 16:50:46 -0600

    Crane animation randomizer

---
+++ Assets/Scripts/AnimationSpeedRandomizer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class AnimationSpeedRandomizer : MonoBehaviour
+{
+	void Start ()
+    {
+        GetComponent<Animator>().speed *= Random.Range(.5f, 1f);
+	}
+}
