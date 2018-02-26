046162b0dab1834ccdd6d2fb4922f3fb4f096aaf
Eric Mouledoux <EricMouledoux@gmail.com>
2018-02-26 11:10:47 -0600

    Start button

---
+++ Assets/Scripts/TimeDelay.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeDelay : MonoBehaviour
+{
+    public float m_TimeDelay;
+    public UnityEngine.Events.UnityEvent m_DelayedEvent;
+
+    public void StartDelayEvent()
+    {
+        StopAllCoroutines();
+        StartCoroutine(Wait());
+    }
+
+    public IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(m_TimeDelay);
+        m_DelayedEvent.Invoke();
+    }
+}
