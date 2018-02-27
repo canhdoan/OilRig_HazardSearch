50ba230a07789f542f6fd5d15f0f6ab57e9993c9
Eric Mouledoux <EricMouledoux@gmail.com>
2018-02-27 11:00:07 -0600

    Reset button WIP

---
+++ Assets/Scripts/ButtonHold.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class ButtonHold : MonoBehaviour
+{
+    private Button m_button;
+    public float m_holdTime;
+    private float m_timer;
+    public bool m_rapidReset;
+
+	void Start ()
+    {
+        m_button = GetComponent<Button>();
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
