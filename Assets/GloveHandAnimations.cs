d5d063f51d380ff05c5e55d9fca1431d35ed7b66
Eric Mouledoux <EricMouledoux@gmail.com>
2018-02-28 17:17:32 -0600

    Glove point animations

---
+++ Assets/GloveHandAnimations.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GloveHandAnimations : MonoBehaviour
+{
+    private Valve.VR.InteractionSystem.Hand m_parentHand;
+    private Animator m_animator;
+
+    void Start ()
+    {
+        m_parentHand = GetParentHand(gameObject);
+        m_animator = GetComponent<Animator>();
+	}
+	
+	void Update ()
+    {
+        m_animator.SetBool("Pointing", m_parentHand.GetStandardInteractionButton());
+	}
+
+    private Valve.VR.InteractionSystem.Hand GetParentHand(GameObject childObject)
+    {
+        Valve.VR.InteractionSystem.Hand parentHand = childObject.GetComponentInParent<Valve.VR.InteractionSystem.Hand>();
+
+        if (parentHand != null)
+            return parentHand;
+
+        else
+            return GetParentHand(childObject.transform.parent.gameObject);
+    }
+}
